1f960026953b61198de4b712621122dcb98ae768
Sumit Malviya <sumitindian11@gmail.com>
2020-08-19 23:26:16 +0530

    Second Commit

---
+++ build/precache-manifest.4b7ee7c5bb9e57dc806192f882a2d2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e890080aca464cad950445a8e06d6f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bacfaccfe1b63f437b5",
+    "url": "/static/css/main.3a3e90ea.chunk.css"
+  },
+  {
+    "revision": "9d30a9828e5770509a2c",
+    "url": "/static/js/2.26585795.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.26585795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bacfaccfe1b63f437b5",
+    "url": "/static/js/main.e26b7214.chunk.js"
+  },
+  {
+    "revision": "727a217337628b469e70",
+    "url": "/static/js/runtime-main.32ec105e.js"
+  }
+]);
